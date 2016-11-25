dbe76156cf0f988c2b2039577046c813fc70814f
David Faraci <faraci@gmail.com>
2016-11-25 10:52:57 -0500

    cleanup, changes to mimetype detection, getting encryption working

--- letters.py
+++ letters.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request, g
 from flask.ext.mobility import Mobility
 from flask.ext.mobility.decorators import mobile_template
 from outgoing_email import EmailUtils
-import magic, os, sys, re
+from encryption import f_encrypt
+import os, sys, re
 
 app = Flask(__name__)
 Mobility(app)
@@ -62,21 +63,19 @@ def upload_letter():
         codedfilename = '/home/dev/LettersC-Flask/letters/' + codename + ".pdf"
         metadata = codename + ' ' + rfncode + ' ' + rlncode + ' ' + afncode + ' ' + alncode + ' ' + aem + '\r\n'
         f = request.files['letter']
-        f.save(codedfilename)
-        m = magic.open(magic.MAGIC_MIME)
-        m.load()
-        if m.file(codedfilename) == 'application/pdf; charset=binary':
+        if f.mimetype != 'application/pdf':
+            return render_template('failure.html')
+        else:
             if aem != aec:
                 return render_template('mismatch.html')
             else:
+                key = f_encrypt(codedfilename, f.read())
+                mailto = codename + '_' + key
                 a = open('metadata.txt', 'a')
                 a.write(metadata)
                 a.close()
-                htmlbody = render_template('rec_confirm.html',rfn=rfn,rln=rln,afn=afn,aln=aln,codename=codename)
-                body = 'Dear ' + afn + ' ' + aln + ',' + '\r\n' + '\r\n' + 'MARGY has received a confidential letter of recommendation for you from ' + rfn + ' ' + rln + '. ' + 'To have the letter delivered to {email addresses}, send an email to ' + codename + '@margymail.com' + ' with {email addresses} in the email body. For more detailed instructions, visit http://margymail.com/instructions. If you have further questions, please direct them to admin@margybeta.davidfaraci.com.' + '\r\n' + '\r\n' + 'MARGY'
+                htmlbody = render_template('rec_confirm.html',rfn=rfn,rln=rln,afn=afn,aln=aln,codename=mailto)
+                body = 'Dear ' + afn + ' ' + aln + ',' + '\r\n' + '\r\n' + 'MARGY has received a confidential letter of recommendation for you from ' + rfn + ' ' + rln + '. ' + 'To have the letter delivered to {email addresses}, send an email to ' + mailto + '@margymail.com' + ' with {email addresses} in the email body. For more detailed instructions, visit http://margymail.com/instructions. If you have further questions, please direct them to admin@margybeta.davidfaraci.com.' + '\r\n' + '\r\n' + 'MARGY'
                 EmailUtils.rich_message('MARGY@margy.davidfaraci.com',aem,'Letter Received',body,htmlbody)
                 print "boo"
                 return render_template('success.html',aem=aem,codename=codename)
-        else:
-            os.remove(codedfilename)
-            return render_template('failure.html')
