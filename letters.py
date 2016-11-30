548a035c31de3c5e40dc03a2fd9f9905bbdf6aa5
MARGY <admin@margymail.com>
2016-11-30 17:56:09 +0000

    PayPal Donations Added

--- letters.py
+++ letters.py
@@ -43,6 +43,10 @@ def whitelist():
 def donate():
     return render_template('donate.html')
 
+@app.route('/thanks')
+def thanks():
+    return render_template('thanks.html')
+
 @app.route('/credits')
 def credits():
     return render_template('credits.html')
