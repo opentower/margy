723f11f37f06f8d8186e8871fe0eb0f438fbc92b
Git <faraci@gmail.com>
2016-12-09 13:46:57 -0500

    Corruption testing; more commentary

--- letters.py
+++ letters.py
@@ -7,96 +7,94 @@ import os, sys, re
 
 app = Flask(__name__, static_url_path='')
 Mobility(app)
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 #sets max size of 16 MB for uploads
 
-@app.route('/')
+@app.route('/') #handles requests for http://margymail.com
 def home():
     return render_template('home.html')
 
-@app.route('/upload')
+@app.route('/upload') #handles requests for http://margymail.com/upload
 def newletter():
     return render_template('upload.html')
 
-@app.errorhandler(413)
+@app.errorhandler(413) #renders error template for oversized files
 def too_big(error):
     return render_template('toobig.html'), 413
 
-@app.route('/instructions')
+@app.route('/instructions') #handles requests for http://margymail.com/instructions
 def instructions():
     return render_template('quick.html')
 
-@app.route('/detailed')
+@app.route('/detailed') #handles requests for Detailed Instructions from within http://margymail.com/instructions
 def detailed():
     return render_template('detailed.html')
 
-@app.route('/howto')
+@app.route('/howto') #handles requests for How To... from within http://margymail.com/instructions
 def questions():
     return render_template('howto.html')
 
-@app.route('/confidentiality')
+@app.route('/confidentiality') #handles requests for http://margymail.com/confidentiality
 def whitelist():
-    with open('static/whitelist.txt', 'r') as f:
+    with open('static/whitelist.txt', 'r') as f: #gets the contents of whitelist.txt so they can be displayed
         data = f.read()
     return render_template('confidentiality.html',data=data)
 
-@app.route('/donate')
+@app.route('/donate') #handles requests for http://margymail.com/donate
 def donate():
     return render_template('donate.html')
 
-@app.route('/thanks')
+@app.route('/thanks') #displays message of thanks to donors
 def thanks():
     return render_template('thanks.html')
 
-@app.route('/credits')
+@app.route('/credits') #handles requests for http://margymail.com/credits
 def credits():
     return render_template('credits.html')
 
-@app.route('/logo')
+@app.route('/logo') #static URL for the MARGY logo
 def logo():
     return app.send_static_file('margylogo.png')
 
-@app.route('/signature')
+@app.route('/signature') #static URL for the MARGY signature
 def signature():
     return app.send_static_file('signature.png')
 
 @app.route('/letter', methods=['POST'])
 def upload_letter():
     if request.method == 'POST':
-        render_template('progress.html')
-        rfn = request.form['rec_fname'].lstrip().rstrip()
-        rln = request.form['rec_lname'].lstrip().rstrip()
-        afn = request.form['app_fname'].lstrip().rstrip()
-        aln = request.form['app_lname'].lstrip().rstrip()
-        aem = request.form['app_email'].lstrip().rstrip()
-        aec = request.form['app_emailc'].lstrip().rstrip()
-        rfncode = rfn.replace(" ", "_")
+        rfn = request.form['rec_fname'].lstrip().rstrip() #assigns input text to variables; this is Recommender's First Name
+        rln = request.form['rec_lname'].lstrip().rstrip() #Recommender's Last Name
+        afn = request.form['app_fname'].lstrip().rstrip() #Applicant's First Name
+        aln = request.form['app_lname'].lstrip().rstrip() #Applicant's Last Name
+        aem = request.form['app_email'].lstrip().rstrip() #Applicant's Email Address
+        aec = request.form['app_emailc'].lstrip().rstrip() #Applicant's Email Address (confirmed)
+        rfncode = rfn.replace(" ", "_") #Replaces spaces with underscores in names
         rlncode = rln.replace(" ", "_")
         afncode = afn.replace(" ", "_")
         alncode = aln.replace(" ", "_")
-        codename = aln + rln
+        codename = aln + rln #Assigns Applicant's Last Name and Recommender's Last Name as the fist part of the letter's file name
         d = open('metadata.txt', 'r')
         num = 0
-        for line in d:
+        for line in d: #checks whether there are other lines in the metadata with the same Last Names combination
             if codename.lower() == line.rstrip().lower()[:len(codename)]:
                 num += 1
-        if num != 0:
+        if num != 0: #if there are n instances of this particular Last Name Combination in the metadata and n > 0, appends the codename with n+1.
             codename += str(num)
-        codedfilename = 'letters/' + codename + ".pdf"
-        metadata = codename + ' ' + rfncode + ' ' + rlncode + ' ' + afncode + ' ' + alncode + ' ' + aem + '\r\n'
+        codedfilename = 'letters/' + codename + ".pdf" #assigns the file a path: letters/[value of codename].pdf
+        metadata = codename + ' ' + rfncode + ' ' + rlncode + ' ' + afncode + ' ' + alncode + ' ' + aem + '\r\n' #creates a line of metadata from all the above
         f = request.files['letter']
-        if f.mimetype != 'application/pdf':
+        if f.mimetype != 'application/pdf': #displays an error if the uploaded file is not a PDF
             return render_template('failure.html')
         else:
-            if aem != aec:
+            if aem != aec: #displays an error if the email addresses do not match
                 return render_template('mismatch.html')
             else:
-                key = f_encrypt(codedfilename, f.read())
-                mailto = codename + '_' + key
+                key = f_encrypt(codedfilename, f.read()) #encrypts the letter and assigns it a decryption key
+                mailto = codename + '_' + key #creates a mailto code, concatenating the codename, an underscore, and the key
                 a = open('metadata.txt', 'a')
-                a.write(metadata)
+                a.write(metadata) #writes the metadata to metadata.txt
                 a.close()
                 txtbody = render_template('rec_confirm.txt',rfn=rfn,rln=rln,afn=afn,aln=aln,codename=mailto)
                 htmlbody = render_template('rec_confirm.html',rfn=rfn,rln=rln,afn=afn,aln=aln,codename=mailto)
-                EmailUtils.rich_message('MARGY@margymail.com',aem,'Letter Received',txtbody,htmlbody)
-                print "boo"
-                return render_template('success.html',aem=aem,codename=codename)
+                EmailUtils.rich_message('MARGY@margymail.com',aem,'Letter Received',txtbody,htmlbody) #sends an email to the applicant with their mailto code
+                return render_template('success.html',aem=aem,codename=codename) #displays a success message to the uploader
--- server.py
+++ server.py
@@ -19,22 +19,118 @@ print 'Starting custom mail server'
 
 #Regexes for processing email
 #-negation of typically forbidden characters, as well as quotation marks, which we stipulate are ridiculous and do not belong in email addresses.
-emailchar = r"[^@ \n\(\)\,\:\;\<\>\[\\\]\"]" 
+emailchar = r"[^@ \n\(\)\,\:\;\<\>\[\\\]\"]"
 #-the actual regex for an address
 email = re.compile(emailchar + r"+@" + emailchar + r"+\." + emailchar + r"+")
 #-address with capture group for the recipient
 recip = re.compile(r"(" + emailchar + r"+)@" + emailchar + r"+\." + emailchar + r"+")
 
-class MargySMTPServer(smtpd.SMTPServer):
+#EMAIL HANDLERS
 
+#Deals with emails sent to whitelist@margymail.com (beta only)
+def whitelist_handler(replyadr):
+    if replyadr not in open('static/whitelist.txt').read():
+        wl = open('static/whitelist.txt', 'a')
+        wl.write(replyadr + '\r\n')
+        EmailUtils.text_message('MARGY@margymail.com',replyadr,'Whitelist Addition','Your email address has been added to MARGY\'s whitelist. Thank you for helping beta test MARGY!')
+        wl.close
+        return
+    else:
+        EmailUtils.text_message('MARGY@margymail.com',replyadr,'Already on Whitelist','A request was received to add this email address to MARGY\'s whitelist, but the address is already on the list. Thank you for helping beta test MARGY!')
+        return
+
+#Forwards emails sent to admin@margymail.com to faraci@gmail.com
+def admin_handler(data,log):
+    EmailUtils.forward_message(data,'faraci@gmail.com')
+    log.write('Forwarded.\r\n')
+    return
+
+#Replies with an error if the mailto code is too short
+def too_short_handler(replyadr,recipient,log):
+    with app.app_context():
+        shorttxt = render_template('code_failure.txt',code=recipient)
+        short = render_template('code_failure.html',code=recipient)
+    EmailUtils.rich_message('MARGY@margymail.com',replyadr,'Letter Delivery Failure',shorttxt,short)
+    log.write(recipient + 'is too short. Failure message sent to ' + replyadr + '.\r\n')
+    return
+
+#Replies with an error if no match is found for the mailto code in metadata.txt
+def no_match_handler(replyadr,recipient,log):
+    with app.app_context():
+        errortxt = render_template('code_failure.txt',code=recipient)
+        error = render_template('code_failure.html',code=recipient)
+    EmailUtils.rich_message('MARGY@margymail.com',replyadr,'Letter Delivery Failure',errortxt,error)
+    log.write(recipient + 'not in metadata. Failure message sent to ' + replyadr + '.\r\n')
+    return
+
+#Replies with an error if no such file is found in letters/
+def no_such_file_handler(replyadr,cfn,log):
+    with app.app_context():
+        nofiletxt = render_template('file_failure.txt',cfn=cfn)
+        nofile = render_template('file_failure.html',cfn=cfn)
+    EmailUtils.rich_message('MARGY@margymail.com',replyadr,'Letter Delivery Failure',nofiletxt,nofile)
+    log.write('File not found. Failure message sent to ' + replyadr + '.\r\n')
+    return
+
+#Replies with an error if the file is corrupt
+def corrupt_file_handler(replyadr,cfn,log):
+    with app.app_context():
+        corrupttxt = render_template('corrupt_failure.txt',file=cfn)
+        corrupt = render_template('corrupt_failure.html',file=cfn)
+    maybepdf.close()
+    EmailUtils.rich_message('MARGY@margymail.com',replyadr,'Letter Delivery Failure',corrupttxt,corrupt)
+    log.write('Corrupt file. Failure message sent to ' + replyadr + '.\r\n')
+    return
+
+#Delivers the letter to a whitelisted address
+def delivery_handler(match,rfn,rln,afn,aln,attach,cfn,log):
+    with app.app_context():
+       toedutxt = render_template('delivery.txt',rfn=rfn,rln=rln,afn=afn,aln=aln)
+       toedu = render_template('delivery.html',rfn=rfn,rln=rln,afn=afn,aln=aln)
+    applicant = afn + ' ' + aln
+    subject = 'Letter Delivery for ' + applicant
+    EmailUtils.rich_message('MARGY@margymail.com',match,subject,toedutxt,toedu,attach,cfn)
+    log.write('Delivery made to ' + match + '.\r\n')
+    return
+
+#Replies with an error if there are no whitelisted addresses present
+def not_whitelisted_handler(replyadr,log):
+    with app.app_context():
+       wlfailtxt = render_template('wl_failure.txt')
+       wlfail = render_template('wl_failure.html')
+    EmailUtils.rich_message('MARGY@margymail.com',replyadr,'Letter Delivery Failure',wlfailtxt,wlfail)
+    log.write('No whitelisted addresses present. Failure message sent to ' + replyadr + '.\r\n')
+    return
+
+#Sends the applicant a confirmation noting whitelisted addresses sent to and any requested addresses not sent to
+def applicant_confirmation_handler(aem,rfn,rln,afn,aln,cfn,sentto,fsent,log):
+    with app.app_context():
+        toapptxt = render_template('del_confirm.txt',rfn=rfn,rln=rln,afn=afn,aln=aln,cfn=cfn,sentto=sentto,failed=fsent)
+        toapp = render_template('del_confirm.html',rfn=rfn,rln=rln,afn=afn,aln=aln,cfn=cfn,sentto=sentto,failed=fsent)
+    EmailUtils.rich_message('MARGY@margymail.com',aem,'Letter Delivery Confirmation',toapptxt,toapp)
+    log.write('Confirmation sent to ' + aem + '.\r\n')
+    return
+
+#Replies with a confirmation noting whitelisted addresses sent to and any requested addresses not sent to
+def sender_confirmation_handler(replyadr,rfn,rln,afn,aln,cfn,sentto,fsent,log):
+    with app.app_context():
+        tosendertxt = render_template('del_confirm.txt',rfn=rfn,rln=rln,afn=afn,aln=aln,cfn=cfn,sentto=sentto,failed=fsent)
+        tosender = render_template('del_confirm.html',rfn=rfn,rln=rln,afn=afn,aln=aln,cfn=cfn,sentto=sentto,failed=fsent)
+    EmailUtils.rich_message('MARGY@margymail.com',replyadr,'Letter Delivery Confirmation',tosendertxt,tosender)
+    log.write('Confirmation sent to ' + replyadr + '.\r\n')
+    return
+
+#SERVER
+
+class MargySMTPServer(smtpd.SMTPServer):
     def process_message(self, peer, mailfrom, rcpttos, data):
         log = open('serverlog.txt', 'a') #The log will be removed once MARGY is out of beta.
         log.write('\r\n')
         print 'Receiving...'
         #log the incoming message
-        log.write('Receiving message from:' + str(peer))
-        log.write('Message addressed from:' + str(mailfrom))
-        log.write('Message addressed to  :' + str(rcpttos))
+        log.write('Receiving message from:' + str(peer) + '\r\n' )
+        log.write('Message addressed from:' + str(mailfrom) + '\r\n' )
+        log.write('Message addressed to  :' + str(rcpttos) + '\r\n' )
         log.write('Message length        :' + str(len(data)) + '\r\n' + '\r\n')
         parser = Parser()
         msg = parser.parsestr(data)
@@ -44,107 +140,71 @@ class MargySMTPServer(smtpd.SMTPServer):
                 matches = matches + re.findall(email,part.get_payload())
         matches = list(set(matches)) #deduplicate match list
         if 'reply-to' in msg: replyadr = msg['reply-to']
-        else: replyadr = str(mailfrom)
+        else: replyadr = str(mailfrom) #checks for a reply-to address; if not present, assigns sender as reply-to address
         for addr in rcpttos: #handle multiple addresses
 	    recipient = re.match(recip, addr).group(1)
-            if recipient == 'whitelist':
-                if replyadr not in open('static/whitelist.txt').read():
-                    wl = open('static/whitelist.txt', 'a')
-                    wl.write(replyadr + '\r\n')
-                    EmailUtils.text_message('MARGY@margymail.com',replyadr,'Whitelist Addition','Your email address as been added to MARGY\'s whitelist. Thank you for helping beta test MARGY!')
-                    wl.close
-                else:
-                    EmailUtils.text_message('MARGY@margymail.com',replyadr,'Already on Whitelist','A request was received to add this email address to MARGY\'s whitelist, but the address is already on the list. Thank you for helping beta test MARGY!')
+            if recipient.lower() == 'whitelist': #for emails sent to whitelist@margymail.com (beta only)
+                whitelist_handler(replyadr)
             else:
-                if recipient == 'admin':
-                    EmailUtils.forward_message(data,'faraci@gmail.com')
-                    log.write('Forwarded.\r\n')
+                if recipient.lower() == 'admin': #for emails sent to admin@margymail.com
+                    admin_handler(data,log)
                 else:
-                    if len(recipient) < 11:
-                        with app.app_context():
-                            shorttxt = render_template('code_failure.txt',code=recipient)
-                            short = render_template('code_failure.html',code=recipient)
-                        EmailUtils.rich_message('MARGY@margymail.com',replyadr,'Letter Delivery Failure',shorttxt,short)
-                        log.write(recipient + 'is too short. Failure message sent to ' + replyadr + '.\r\n')
+                    if len(recipient) < 11: #makes sure the mailto code isn't too short
+                        too_short_handler(replyadr,recipient,log)
                     else:
-                        filecode = recipient[:-10]
-                        key = recipient[-9:]
-                        f = open('metadata.txt', 'r')
-                        mdata = 'empty'
-                        for line in f:
+                        filecode = recipient[:-10] #scrapes all but the last 10 characters of the mailto code to serve as filecode (e.g., 'Test' from Test_123456789)
+                        key = recipient[-9:] #scrapes the final 9 characters of the mailto code to serve as key (e.g., '123456789' from Test_123456789)
+                        f = open('metadata.txt', 'r') #opens the file containing the metadata
+                        mdata = 'empty' #set in case there is no metadata for the filecode
+                        for line in f: #checks whether the filecode matches the first entry on each line of metadata and, if so, scrapes the line
                             if filecode.lower() == line.rstrip().lower()[:len(filecode)]:
-                                mdata = line
+                                mdata = line #
                                 break
                         f.close()
-                        if mdata == 'empty':
-                            with app.app_context():
-                                errortxt = render_template('code_failure.txt',code=recipient)
-                                error = render_template('code_failure.html',code=recipient)
-                            EmailUtils.rich_message('MARGY@margymail.com',replyadr,'Letter Delivery Failure',errortxt,error)
-                            log.write(recipient + 'not in metadata. Failure message sent to ' + replyadr + '.\r\n')
+                        if mdata == 'empty': #deals with cases where there is no metadata for the filecode
+                            no_match_handler(replyadr,recipient,log)
                         else:
-                            array = mdata.split()
-                            cfn = array[0] + '.pdf'
-                            path = 'letters/' + cfn + '.aes'
-                            if not os.path.isfile(path):
-                                with app.app_context():
-                                    nofiletxt = render_template('file_failure.txt',cfn=cfn)
-                                    nofile = render_template('file_failure.html',cfn=cfn)
-                                EmailUtils.rich_message('MARGY@margymail.com',replyadr,'Letter Delivery Failure',nofiletxt,nofile)
-                                log.write('File not found. Failure message sent to ' + replyadr + '.\r\n')
+                            array = mdata.split() #creates a list out of the relevant line of metadata
+                            cfn = array[0] + '.pdf' #assigns the first item in the metadata list as the file name (should be the same as filecode)
+                            path = 'letters/' + cfn + '.aes' #the path to the file to be attached should be letters/[value of cfn].aes
+                            if not os.path.isfile(path): #checks whether there is such a file
+                                no_such_file_handler(replyadr,cfn,log)
                             else:
-                                attach = f_decrypt(path, key)
-				maybepdf = StringIO.StringIO(attach)
-				try:
-				   PyPDF2.PdfFileReader(maybepdf)
-				except PyPDF2.utils.PdfReadError:
-				   print "Abort, corrupt pdf!" 
-				   #insert email explaining that this is not the correct keycode here.
-				   maybepdf.close()
-				else:
-				   maybepdf.close()
-                                   rfn = array[1].replace('_', ' ')
-                                   rln = array[2].replace('_', ' ')
-                                   afn = array[3].replace('_', ' ')
-                                   aln = array[4].replace('_', ' ')
-                                   aem = array[5]
-                                   with app.app_context():
-                                       toedutxt = render_template('delivery.txt',rfn=rfn,rln=rln,afn=afn,aln=aln)
-                                       toedu = render_template('delivery.html',rfn=rfn,rln=rln,afn=afn,aln=aln)
-                                   sentto = ""
-                                   failed = []
-                                   for match in matches + [replyadr]:
+                                attach = f_decrypt(path, key) #assigns decrypted file as attachment
+                                maybepdf = StringIO.StringIO(attach) #checks to make sure the file isn't corrupt
+                                try:
+                                    PyPDF2.PdfFileReader(maybepdf)
+                                except PyPDF2.utils.PdfReadError:
+                                    corrupt_file_handler(replyadr,cfn,log)
+                                else:
+                                    maybepdf.close()
+                                    rfn = array[1].replace('_', ' ') #assigns the items of the metadata list to variables; this is recommender's first name
+                                    rln = array[2].replace('_', ' ') #recommender's last name
+                                    afn = array[3].replace('_', ' ') #applicant's first name
+                                    aln = array[4].replace('_', ' ') #applicant's last name
+                                    aem = array[5] #applicant's email address
+                                    sentto = "" #creates an empty variable for whitelisted addresses that will receive attachment
+                                    failed = [] #creates an empty list for non-whitelisted addresses that will not receive attachment
+                                    for match in matches + [replyadr]: #checks each email address in the body and the reply-to address against the whitelist
                                        wl = open('static/whitelist.txt')
                                        for line in wl:
                                            if ( match.lower() == line.rstrip().lower() and match not in sentto.strip().lower() ):
-                                               applicant = afn + ' ' + aln
-                                               subject = 'Letter Delivery for ' + applicant
-                                               EmailUtils.rich_message('MARGY@margymail.com',match,subject,toedutxt,toedu,attach,cfn)
-                                               sentto += match
-                                               log.write('Delivery made to ' + match + '.\r\n')
+                                               delivery_handler(match,rfn,rln,afn,aln,attach,cfn,log)
+                                               sentto += match + ' ' #adds to the variable that contains whitelisted addresses that have been sent attachment
                                            else:
-                                               failed.append(match)
+                                               failed.append(match) #adds to the list of non-whitelisted addresses that have not been sent attachment
                                        wl.close()
-                                   fsent = ""
-                                   for miss in failed:
-                                       if ( miss.lower() != replyadr.lower() and miss.lower() not in sentto.strip().lower() and miss.lower() not in fsent.strip().lower() ):
-                                           fsent += miss + ' '
-                                           log.write('Delivery failed to ' + line + '.\r\n')
-                                   with app.app_context():
-                                       toapptxt = render_template('del_confirm.txt',rfn=rfn,rln=rln,afn=afn,aln=aln,cfn=cfn,sentto=sentto,failed=fsent)
-                                       toapp = render_template('del_confirm.html',rfn=rfn,rln=rln,afn=afn,aln=aln,cfn=cfn,sentto=sentto,failed=fsent)
-                                       wlfailtxt = render_template('wl_failure.txt')
-                                       wlfail = render_template('wl_failure.html')
-                                   if sentto == "":
-                                       EmailUtils.rich_message('MARGY@margymail.com',replyadr,'Letter Delivery Failure',wlfailtxt,wlfail)
-                                       log.write('No whitelisted addresses present. Failure message sent to ' + replyadr + '.\r\n')
-                                   else:
-                                       if aem.lower() not in sentto.strip().lower():
-                                           EmailUtils.rich_message('MARGY@margymail.com',aem,'Letter Delivery Confirmation',toapptxt,toapp)
-                                           log.write('Confirmation sent to ' + aem + '.\r\n')
-                                       if ( replyadr.lower() != aem.lower() and replyadr.lower() not in sentto.strip().lower() ):
-                                           EmailUtils.rich_message('MARGY@margymail.com',replyadr,'Letter Delivery Confirmation',toapptxt,toapp)
-                                           log.write('Confirmation sent to ' + replyadr + '.\r\n')
+                                    fsent = "" #creates an empty variable for...
+                                    for miss in failed: #...deduplication and removal of reply-to address from the list of failed recipients
+                                        if ( miss.lower() != replyadr.lower() and miss.lower() not in sentto.strip().lower() and miss.lower() not in fsent.strip().lower() ):
+                                            fsent += miss + ' '
+                                            log.write('Delivery failed to ' + line + '.\r\n')
+                                    if sentto == "": #deals with cases where there were no whitelisted addresses
+                                        not_whitelisted_handler(replyadr,log)
+                                    else: #sends confirmations to the applicant and the reply-to address (if attachment was sent anywhere)
+                                        applicant_confirmation_handler(aem,rfn,rln,afn,aln,cfn,sentto,fsent,log)
+                                        if ( replyadr.lower() != aem.lower()):
+                                           sender_confirmation_handler(replyadr,rfn,rln,afn,aln,cfn,sentto,fsent,log)
         log.write('End of log entry.')
         log.close()
         print 'Done.'
