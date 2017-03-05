589690733f78662f9fcf3a890b37536b91820402
Git <faraci@gmail.com>
2017-03-05 17:11:58 -0500

    add spam blacklisting

--- server.py
+++ server.py
@@ -123,72 +123,79 @@ class MargySMTPServer(smtpd.SMTPServer):
         matches = list(set(matches)) #deduplicate match list
         if 'reply-to' in msg: replyadr = msg['reply-to']
         else: replyadr = str(mailfrom) #checks for a reply-to address; if not present, assigns sender as reply-to address
-        for addr in rcpttos: #handle multiple addresses
-	    recipient = re.match(recip, addr).group(1)
-            if ( recipient.lower() == 'admin' or recipient.lower() == 'postmaster' or recipient.lower() == 'abuse' or recipient.lower() == 'margy' ): #for emails sent to admin@ or postmaster@ or abuse@ or MARGY@margymail.com
-                admin_handler(data,log)
-            else:
-                if len(recipient) < 11: #makes sure the mailto code isn't too short
-                    too_short_handler(replyadr,recipient,log)
+        bl = open('blacklist', 'r') #open blacklist (spam avoidance)
+        blisted = 0
+        for line in bl: #checks reply-to address against blacklist
+            if line.lower() == replyadr.lower():
+                log.write('Email blocked from blacklisted address ' + replyadr + '.')
+                blisted = 1
+        if blisted = 0:
+            for addr in rcpttos: #handle multiple addresses
+    	    recipient = re.match(recip, addr).group(1)
+                if ( recipient.lower() == 'admin' or recipient.lower() == 'postmaster' or recipient.lower() == 'abuse' or recipient.lower() == 'margy' ): #for emails sent to admin@ or postmaster@ or abuse@ or MARGY@margymail.com
+                    admin_handler(data,log)
                 else:
-                    filecode = recipient[:-10] #scrapes all but the last 10 characters of the mailto code to serve as filecode (e.g., 'Test' from Test_123456789)
-                    key = recipient[-9:] #scrapes the final 9 characters of the mailto code to serve as key (e.g., '123456789' from Test_123456789)
-                    f = open('metadata.txt', 'r') #opens the file containing the metadata
-                    mdata = 'empty' #set in case there is no metadata for the filecode
-                    for line in f: #checks whether the filecode matches the first entry on each line of metadata and, if so, scrapes the line
-                        if filecode.lower() == line.rstrip().lower()[:len(filecode)]:
-                            mdata = line #
-                            break
-                    f.close()
-                    if mdata == 'empty': #deals with cases where there is no metadata for the filecode
-                        no_match_handler(replyadr,recipient,log)
+                    if len(recipient) < 11: #makes sure the mailto code isn't too short
+                        too_short_handler(replyadr,recipient,log)
                     else:
-                        array = mdata.split() #creates a list out of the relevant line of metadata
-                        cfn = array[0] + '.pdf' #assigns the first item in the metadata list as the file name (should be the same as filecode)
-                        path = 'letters/' + cfn + '.aes' #the path to the file to be attached should be letters/[value of cfn].aes
-                        if not os.path.isfile(path): #checks whether there is such a file
-                            no_such_file_handler(replyadr,cfn,log)
+                        filecode = recipient[:-10] #scrapes all but the last 10 characters of the mailto code to serve as filecode (e.g., 'Test' from Test_123456789)
+                        key = recipient[-9:] #scrapes the final 9 characters of the mailto code to serve as key (e.g., '123456789' from Test_123456789)
+                        f = open('metadata.txt', 'r') #opens the file containing the metadata
+                        mdata = 'empty' #set in case there is no metadata for the filecode
+                        for line in f: #checks whether the filecode matches the first entry on each line of metadata and, if so, scrapes the line
+                            if filecode.lower() == line.rstrip().lower()[:len(filecode)]:
+                                mdata = line #
+                                break
+                        f.close()
+                        if mdata == 'empty': #deals with cases where there is no metadata for the filecode
+                            no_match_handler(replyadr,recipient,log)
                         else:
-                            attach = f_decrypt(path, key) #assigns decrypted file as attachment
-                            maybepdf = StringIO.StringIO(attach) #checks to make sure the file isn't corrupt
-                            try:
-                                PyPDF2.PdfFileReader(maybepdf)
-                            except PyPDF2.utils.PdfReadError:
-                                corrupt_file_handler(replyadr,cfn,log)
-                                maybepdf.close()
+                            array = mdata.split() #creates a list out of the relevant line of metadata
+                            cfn = array[0] + '.pdf' #assigns the first item in the metadata list as the file name (should be the same as filecode)
+                            path = 'letters/' + cfn + '.aes' #the path to the file to be attached should be letters/[value of cfn].aes
+                            if not os.path.isfile(path): #checks whether there is such a file
+                                no_such_file_handler(replyadr,cfn,log)
                             else:
-                                maybepdf.close()
-                                rfn = array[1].replace('_', ' ') #assigns the items of the metadata list to variables; this is recommender's first name
-                                rln = array[2].replace('_', ' ') #recommender's last name
-                                afn = array[3].replace('_', ' ') #applicant's first name
-                                aln = array[4].replace('_', ' ') #applicant's last name
-                                aem = array[5] #applicant's email address
-                                sentto = "" #creates an empty variable for whitelisted addresses that will receive attachment
-                                failed = [] #creates an empty list for non-whitelisted addresses that will not receive attachment
-                                for match in matches + [replyadr]: #checks each email address in the body and the reply-to address against the whitelist
-                                   wl = open('static/whitelist.txt')
-                                   for line in wl:
-                                       if ( match.lower() == line.rstrip().lower() and match not in sentto.strip().lower() ):
-                                           delivery_handler(match,rfn,rln,afn,aln,attach,cfn,log)
-                                           sentto += match + ' ' #adds to the variable that contains whitelisted addresses that have been sent attachment
-                                       else:
-                                           failed.append(match) #adds to the list of non-whitelisted addresses that have not been sent attachment
-                                   wl.close()
-                                fsent = "" #creates an empty variable for...
-                                for miss in failed: #...deduplication and removal of reply-to address from the list of failed recipients
-                                    if ( miss.lower() != replyadr.lower() and miss.lower() not in sentto.strip().lower() and miss.lower() not in fsent.strip().lower() ):
-                                        fsent += miss + ' '
-                                        log.write('Delivery failed to ' + line + '.\r\n')
-                                if sentto == "": #deals with cases where there were no whitelisted addresses
-                                    not_whitelisted_handler(replyadr,log)
-                                else: #sends confirmations to the applicant and the reply-to address (if attachment was sent anywhere)
-                                    applicant_confirmation_handler(aem,rfn,rln,afn,aln,cfn,sentto,fsent,log)
-                                    if ( replyadr.lower() != aem.lower()):
-                                       sender_confirmation_handler(replyadr,rfn,rln,afn,aln,cfn,sentto,fsent,log)
-        log.write('End of log entry.')
-        log.close()
-        print 'Done.'
-        return
+                                attach = f_decrypt(path, key) #assigns decrypted file as attachment
+                                maybepdf = StringIO.StringIO(attach) #checks to make sure the file isn't corrupt
+                                try:
+                                    PyPDF2.PdfFileReader(maybepdf)
+                                except PyPDF2.utils.PdfReadError:
+                                    corrupt_file_handler(replyadr,cfn,log)
+                                    maybepdf.close()
+                                else:
+                                    maybepdf.close()
+                                    rfn = array[1].replace('_', ' ') #assigns the items of the metadata list to variables; this is recommender's first name
+                                    rln = array[2].replace('_', ' ') #recommender's last name
+                                    afn = array[3].replace('_', ' ') #applicant's first name
+                                    aln = array[4].replace('_', ' ') #applicant's last name
+                                    aem = array[5] #applicant's email address
+                                    sentto = "" #creates an empty variable for whitelisted addresses that will receive attachment
+                                    failed = [] #creates an empty list for non-whitelisted addresses that will not receive attachment
+                                    for match in matches + [replyadr]: #checks each email address in the body and the reply-to address against the whitelist
+                                       wl = open('static/whitelist.txt')
+                                       for line in wl:
+                                           if ( match.lower() == line.rstrip().lower() and match not in sentto.strip().lower() ):
+                                               delivery_handler(match,rfn,rln,afn,aln,attach,cfn,log)
+                                               sentto += match + ' ' #adds to the variable that contains whitelisted addresses that have been sent attachment
+                                           else:
+                                               failed.append(match) #adds to the list of non-whitelisted addresses that have not been sent attachment
+                                       wl.close()
+                                    fsent = "" #creates an empty variable for...
+                                    for miss in failed: #...deduplication and removal of reply-to address from the list of failed recipients
+                                        if ( miss.lower() != replyadr.lower() and miss.lower() not in sentto.strip().lower() and miss.lower() not in fsent.strip().lower() ):
+                                            fsent += miss + ' '
+                                            log.write('Delivery failed to ' + line + '.\r\n')
+                                    if sentto == "": #deals with cases where there were no whitelisted addresses
+                                        not_whitelisted_handler(replyadr,log)
+                                    else: #sends confirmations to the applicant and the reply-to address (if attachment was sent anywhere)
+                                        applicant_confirmation_handler(aem,rfn,rln,afn,aln,cfn,sentto,fsent,log)
+                                        if ( replyadr.lower() != aem.lower()):
+                                           sender_confirmation_handler(replyadr,rfn,rln,afn,aln,cfn,sentto,fsent,log)
+            log.write('End of log entry.')
+            log.close()
+            print 'Done.'
+            return
 
 #Listen to port 25 ( 0.0.0.0 can be replaced by the ip of your server but that will work with 0.0.0.0 )
 server = MargySMTPServer(('0.0.0.0', 25), None)
